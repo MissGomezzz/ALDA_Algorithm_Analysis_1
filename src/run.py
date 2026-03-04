d5c4063926cc04092840ec94f08def162960e55d
MissGomezzz <sofiangelagv@gmail.com>
2026-03-04 15:09:54 -0500

    Updating README with more explanations and pics

--- src/run.py
+++ src/run.py
@@ -1,6 +1,22 @@
 from src.execution_time_gathering import take_execution_time
 from src.plot_results import plot_execution_times
+from src.data_generator import (
+    get_random_list,
+    get_sorted_list,
+    get_reverse_sorted_list,
+    get_nearly_sorted_list,
+)
 
 if __name__ == "__main__":
-    results = take_execution_time(minimum_size=100, maximum_size=1000, step=100, samples_by_size=5)
-    plot_execution_times(results)
+
+    random_results = take_execution_time(100, 1000, 100, 5, get_random_list)
+    plot_execution_times(random_results)
+
+    sorted_results = take_execution_time(100, 1000, 100, 5, get_sorted_list)
+    plot_execution_times(sorted_results)
+
+    reverse_results = take_execution_time(100, 1000, 100, 5, get_reverse_sorted_list)
+    plot_execution_times(reverse_results)
+
+    nearly_results = take_execution_time(100, 1000, 100, 5, get_nearly_sorted_list)
+    plot_execution_times(nearly_results)
